c7bfc4b6ce2d3873706cff32d70b3c31f7ce4afa
Eric Bolikowski <eric@binarylights.com>
2024-07-12 18:40:54 +0200

    latest changes

--- functions/index.js
+++ functions/index.js
@@ -20,68 +20,30 @@ exports.token = onRequest({cors: true}, async (req, res) => {
   const {code} = req.body;
   const accessToken = await fetchSpotifyAccessToken(code);
   const songs = await fetchUserSongs(accessToken);
-  console.log(JSON.stringify(songs, null, 2));
-
-  res.json({foo: "bar"});
-  return;
-
-  /*
-  const {code} = req.body;
-  const authString = Buffer.from(`${SPOTIFY_CLIENT_ID}:${SPOTIFY_CLIENT_SECRET}`).toString("base64");
-  const body = qs.stringify({
-    code,
-    redirect_uri: req.headers.origin,
-    grant_type: "authorization_code",
-  });
-  const headers = {
-    "authorization": `Basic ${authString}`,
-    "content-type": "application/x-www-form-urlencoded;charset=utf-8",
-  };
-  try {
-    const response = await axios.post("https://accounts.spotify.com/api/token", body, {headers});
-    const data = response.data;
-    const {access_token, refresh_token} = data;
-    try {
-      const spotifyResponse = await axios.get("https://api.spotify.com/v1/me", {
-        headers: {
-          Authorization: `Bearer ${access_token}`,
-        },
-      });
-    } catch (error) {
-      console.error("Error fetching Spotify user profile:", error.message);
-      res.status(500).json({
-        error: "Failed to fetch Spotify user profile",
-        details: error.message,
-      });
-      return;
-    }
-    const spotifyUserProfile = spotifyResponse.data;
-    const id = spotifyUserProfile.id;
-    const firebase_token = await getAuth().createCustomToken(id);
-    res.json({access_token, refresh_token, firebase_token});
-  } catch (err) {
-    console.error("Error while requesting a token", err);
-    res.status(500).json({
-      error: err.message,
-    });
-  }
-  */
+  console.log(songs);
+  res.status(200).json({songs});
 });
 
 const fetchSpotifyAccessToken = async (code) => {
   try {
-    const response = await axios.post(SPOTIFY_TOKEN_URL, new URLSearchParams({
-      grant_type: "authorization_code",
-      code,
-      redirect_uri: SPOTIFY_REDIRECT_URI,
-    }), {
+    const response = await fetch(SPOTIFY_TOKEN_URL, {
+      method: "POST",
       headers: {
         "Content-Type": "application/x-www-form-urlencoded",
         "Authorization": "Basic " + Buffer.from(`${SPOTIFY_CLIENT_ID}:${SPOTIFY_CLIENT_SECRET}`).toString("base64"),
       },
+      body: new URLSearchParams({
+        grant_type: "authorization_code",
+        code,
+        redirect_uri: SPOTIFY_REDIRECT_URI,
+      }),
     });
 
-    const data = response.data;
+    if (!response.ok) {
+      throw new Error(`Error fetching Spotify access token: ${response.statusText}`);
+    }
+
+    const data = await response.json();
     console.log("Access Token:", data.access_token);
 
     return data.access_token;
@@ -92,13 +54,17 @@ const fetchSpotifyAccessToken = async (code) => {
 };
 
 const fetchUserSongs = async (accessToken) => {
-  const response = await axios.get("https://api.spotify.com/v1/me/tracks", {
+  const response = await fetch("https://api.spotify.com/v1/me/tracks", {
     headers: {
       Authorization: `Bearer ${accessToken}`,
     },
   });
 
-  const data = response.data;
+  if (!response.ok) {
+    throw new Error(`Error fetching user songs: ${response.statusText}`);
+  }
+
+  const data = await response.json();
 
   return data;
 };
