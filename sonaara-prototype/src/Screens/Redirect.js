a2d21bbae72558a1bcffb1a8c3a95fb146f0482c
Eric Bolikowski <eric@binarylights.com>
2024-08-17 12:51:43 +0200

    handle exceptions on /redirect path

--- sonaara-prototype/src/Screens/Redirect.js
+++ sonaara-prototype/src/Screens/Redirect.js
@@ -1,8 +1,9 @@
 import React, { useEffect } from "react";
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 const Redirect = () => {
   const [searchParams] = useSearchParams();
+  const navigate = useNavigate()
   const code = searchParams.get("code");
 
   useEffect(() => {
@@ -14,6 +15,7 @@ const Redirect = () => {
   return (
     <div className="App">
       <h1>Redirecting...</h1>
+      <button onClick={() => navigate('/')}>Go to home (/)</button>
     </div>
   );
 };
@@ -22,26 +24,30 @@ let requestSent = false
 const sendTokenRequest = async (code) => {
   if (requestSent) return;
   requestSent = true;
-  const response = await fetch(
-    "http://127.0.0.1:5001/sonaara-21356/us-central1/token",
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ code }),
-    }
-  );
-  const data = await response.json();
-  console.log(data);
-  const trackDetails = data.songs.items.map((item) => {
-    return {
-      trackName: item.track.name,
-      artistName: item.track.artists[0].name,
-      duration_ms: item.track.duration_ms,
-    };
-  });
-  console.log(trackDetails);
+  try {
+    const response = await fetch(
+      "http://127.0.0.1:5001/sonaara-21356/us-central1/token",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ code }),
+      }
+    );
+    const data = await response.json();
+    console.log(data);
+    const trackDetails = data.songs.items.map((item) => {
+      return {
+        trackName: item.track.name,
+        artistName: item.track.artists[0].name,
+        duration_ms: item.track.duration_ms,
+      };
+    });
+    console.log(trackDetails);
+  } catch (err) {
+    console.log('error occurred', err)
+  }
 };
 
 export default Redirect;
